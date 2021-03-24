fb71e3bca7c9da4b42cbde06c5e45618e879ea9b
Ollie <oliverb3606@sd61learn.ca>
2021-03-23 19:31:18 -0700

    Python Connection to SQL up and running, row insertion working as well.

--- New SQL Testing.py
+++ New SQL Testing.py
@@ -1,13 +1,21 @@
 import mysql.connector
 
+Name = input("Name pwease")
+EndValue = int(input("End Value"))
+Guesses = int(input("Guesses"))
+
 mydb = mysql.connector.connect(
   host="localhost",
-  user="Ollie",
+  user="root",
   password="583606Banana",
-  database="highscores"
+  database="high_scores"
 )
 
 mycursor = mydb.cursor()
+
+mycursor.execute("INSERT INTO high_scores (Name,EndValue,Guesses) VALUES ('%s', %d, %d)" % (Name, EndValue, Guesses))
+
+mydb.commit()
 
 mycursor.execute("SELECT * FROM high_scores")
 
---
+++ Percent Test.py
@@ -0,0 +1,5 @@
+Name = input("Name pwease")
+EndValue = int(input("End Value"))
+Guesses = int(input("Guesses"))
+
+print ("Great job %s! Your range was 1 - %d, and you took %d guesses." % (Name, EndValue, Guesses))
