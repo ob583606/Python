6acb5f431decaf20f7b15459d15fcd184cc65961
ob583606 <50464245+ob583606@users.noreply.github.com>
2021-10-06 19:51:19 -0700

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -4,8 +4,8 @@ spc = ("     ")
 
 while (programLoop != "n"):
     gridLoop = 0
-    gridLength = input("Please type the number of variables for your matrix. (Values above 0 and below 5 are accepted)")
-    gridWidth = input("Please type the number of rows for your matrix. (Values above 0 and below 5 are accepted)")
+    gridLength = input("Please type the number of variables for your matrix. (Values above 0 and below 7 are accepted)")
+    gridWidth = input("Please type the number of rows for your matrix. (Values above 0 and below 7 are accepted)")
 
     try:
         gridLength = int(gridLength)
@@ -15,10 +15,12 @@ while (programLoop != "n"):
     except ValueError:
         print("Sorry, one or more inputs were invalid.")
 
-    if (isinstance(gridLength, int) == True and isinstance(gridWidth, int) == True and gridLength > 0 and gridLength < 5 and gridWidth > 0 and gridWidth < 5):
+    if (isinstance(gridLength, int) == True and isinstance(gridWidth, int) == True and gridLength > 0 and gridLength < 7 and gridWidth > 0 and gridWidth < 7):
         while (gridLoop < gridWidth):
 
             c = random.randint(0, 9)
+            u = random.randint(0, 9)
+            v = random.randint(0, 9)
             w = random.randint(0, 9) 
             x = random.randint(0, 9)
             y = random.randint(0, 9)
@@ -43,5 +45,15 @@ while (programLoop != "n"):
 
                 print(w, spc, x, spc, y, spc, z, spc, c)
                 gridLoop += 1
+
+            if (gridLength == 5):
+
+                print(v, spc, w, spc, x, spc, y, spc, z, spc, c)
+                gridLoop += 1
             
+            if (gridLength == 6):
+
+                print(u, spc, v, spc, w, spc, x, spc, y, spc, z, spc, c)
+                gridLoop += 1
+
     programLoop = input("Would you like a new matrix? Default = yes, n for no.")
